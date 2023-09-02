5fa9c767c942fcab3c80b6195cf2fa62992f52e5
yunuki <woogie_ing@naver.com>
2023-09-02 23:53:30 +0900

    Initial Commit

---
+++ MVIPractice/MVIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVIPracticeApp.swift
+//  MVIPractice
+//
+//  Created by 윤재욱 on 2023/09/02.
+//
+
+import SwiftUI
+
+@main
+struct MVIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
